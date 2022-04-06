dea4d70b0dd5f151cb05e6fd3aaa8cffb577ce1d
Avihay Swissa <avihay126@gmail.com>
2022-04-07 00:14:22 +0300

    5

--- src/GameScene.java
+++ src/GameScene.java
@@ -8,19 +8,18 @@ public class GameScene extends JPanel {
     private Player player;
     private Stadium stadium;
     private Ball ball;
+    private static int speedGame;
 
 
     public GameScene(int x, int y, int width, int height) {
-        Random random = new Random();
         this.setBackground(Color.green);
         this.setLayout(null);
         this.setBounds(x, y, width, height);
         this.setDoubleBuffered(true);
-        int o = random.nextInt(600);
-        int d = 340;
+        this.speedGame=15;
         this.stadium = new Stadium();
-        this.stadium.goalMovement();
-        this.player = new Player(o, d);
+        this.stadium.goalMovement(speedGame);
+        this.player = new Player(this.getX()+this.getWidth()/2, this.getY()+this.getHeight()/2);
         this.ball = new Ball(this.player.legsX(), this.player.legsY());
         this.gameLoop();
 
@@ -28,51 +27,60 @@ public class GameScene extends JPanel {
 
     private void gameLoop() {
         Thread t1 = new Thread(() -> {
-            KeyControl keyControl = new KeyControl(this.player,this.ball);
-            this.setFocusable(true);
-            this.requestFocus();
-            this.addKeyListener(keyControl);
-            boolean shoot=false;
+            keyControl();
+            boolean shoot = false;
             while (true) {
-                switch (this.player.getDirection()){
+
+                switch (this.player.getDirection()) {
                     case Player.RIGHT:
                         this.player.moveRight();
-                        if (!shoot){
-                            this.ball.dribble(this.player.legsX(), this.player.legsY());
-                        }
                         break;
                     case Player.LEFT:
                         this.player.moveLeft();
-                        if (!shoot){
-                            this.ball.dribble(this.player.legsX(), this.player.legsY());
-                        }
                         break;
                 }
-                if (this.ball.getDirection()==Ball.UP){
-                    shoot=true;
+                if (this.player.getLocation()==this.stadium.getBoundX()+this.stadium.getBoundWidth()-this.player.getBodyWidth()){
+                    this.player.moveLeft();
+                }else if(this.player.getLocation()==this.stadium.getBoundX()){
+                    this.player.moveRight();
+                }
+                if (!shoot) {
+                    this.ball.dribble(this.player.legsX(), this.player.legsY());
+                }
+                if (this.ball.getDirection() == Ball.UP) {
+                    shoot = true;
                     this.ball.goUp();
                 }
-                if (this.ball.getYLocation()==this.stadium.getBoundY()+5){
-                    this.ball.dribble(this.player.legsX(), this.player.legsY());
-                    shoot=false;
+                if (this.ball.getYLocation() == this.stadium.getBoundY() - 5) {
+                    shoot = false;
+                    this.ball.setDirection(Ball.NONE);
                 }
+                if (this.ball.getYLocation()==this.stadium.getBoundY()&&(this.ball.getXLocation()>this.stadium.getGoalX()&&this.ball.getXLocation()<this.stadium.getGoalX()+this.stadium.getGoalWidth())){
+                    this.speedGame-=2;
+                    System.out.println("goal");
 
-
-
-
+                }
                 repaint();
                 try {
-                    Thread.sleep(7);
-            }catch(InterruptedException e){
-                e.printStackTrace();
-            }
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
         t1.start();
     }
 
+    public void keyControl() {
+        KeyControl keyControl = new KeyControl(this.player, this.ball);
+        this.setFocusable(true);
+        this.requestFocus();
+        this.addKeyListener(keyControl);
+    }
 
     public void paintComponent(Graphics g) {
+        Graphics2D graphics=(Graphics2D) g;
+        graphics.setStroke(new BasicStroke(5));
         super.paintComponent(g);
         this.stadium.paint(g);
         this.ball.paint(g);
