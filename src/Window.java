06c379e6e2405ebfcfd9eacd3a67ba0483dcea75
AMITFL0R <92460370+AMITFL0R@users.noreply.github.com>
2022-04-07 11:12:54 +0300

    fixed flags.

--- src/Window.java
+++ src/Window.java
@@ -15,7 +15,7 @@ public class Window extends JFrame {
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setLayout(null);
-        this.setTitle("The fast gate");
+        this.setTitle("The fast gate by A.I.U.A");
         GameScene gameScene = new GameScene(0, 0, WIN_WID , WIN_HEI);
         this.add(gameScene);
     }
